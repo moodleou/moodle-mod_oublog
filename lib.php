406d97cb2f803a5348dd25be0845a0158436a311
Jason Platts <j.platts@open.ac.uk>
2013-01-28 11:42:09 +0000

    OUblog: adding image in personal blog - error message #5774

--- lib.php
+++ lib.php
@@ -903,8 +903,13 @@ function oublog_get_file_info($browser, $areas, $course, $cm, $context, $fileare
  * @param cm_info $cm
  */
 function oublog_cm_info_dynamic(cm_info $cm) {
-    if (!has_capability('mod/oublog:view',
-            get_context_instance(CONTEXT_MODULE,$cm->id))) {
+    $capability = 'mod/oublog:view';
+    if ($cm->course == SITEID && $cm->instance == 1) {
+        // Is global blog (To save DB call we make suspect assumption it is instance 1)?
+        $capability = 'mod/oublog:viewpersonal';
+    }
+    if (!has_capability($capability,
+            context_module::instance($cm->id))) {
         $cm->uservisible = false;
         $cm->set_available(false);
     }
